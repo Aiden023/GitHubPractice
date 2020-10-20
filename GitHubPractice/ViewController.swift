4430b2393f988892495a6b99ff1a50090e020faf
Aiden Overton <aoverton4508@stu.d214.org>
2020-10-20 16:21:07 -0400

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by user182417 on 10/20/20.
+//  Copyright © 2020 user182417. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
